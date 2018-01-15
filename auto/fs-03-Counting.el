5dc0b99b08d055096402640ec42485ac72d6cca1
Stefan Lukits <sediomyle@gmail.com>
2018-01-15 12:31:49 -0800

    fs-03-Counting.tex

---
+++ auto/fs-03-Counting.el
@@ -0,0 +1,35 @@
+(TeX-add-style-hook
+ "fs-03-Counting"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor=dvipsnames")))
+   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "graphicx"
+    "wrapfig"
+    "colortbl"
+    "alltt")
+   (TeX-add-symbols
+    "CourseName"
+    "CourseNumber"
+    "CourseInst")
+   (LaTeX-add-labels
+    "eq:adeiquah"
+    "eq:iepeejae"
+    "eq:eahaikee"
+    "eq:aibireik")
+   (LaTeX-add-xcolor-definecolors
+    "myblue"))
+ :latex)
+
