4ed29cac34906a46c80f6debe24df98fade44755
Stefan Lukits <sediomyle@gmail.com>
2018-02-22 10:58:06 -0800

    fs-07-Sampling.tex

---
+++ auto/fs-07-Sampling.el
@@ -0,0 +1,33 @@
+(TeX-add-style-hook
+ "fs-07-Sampling"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor=dvipsnames")))
+   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "teachbeamer")
+   (LaTeX-add-labels
+    "eq:quaegieg"
+    "eq:iitiloox"
+    "eq:wecoutha"
+    "eq:aunijeeb"
+    "eq:aivashah"
+    "eq:chaghiec"
+    "eq:ooloogha"
+    "eq:yohthoug"
+    "eq:ungeihay"
+    "eq:cohseiph"
+    "eq:euruighu"))
+ :latex)
+
